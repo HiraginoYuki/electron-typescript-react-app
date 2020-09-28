75341245d4cfdfb9b032be2c13b2569926ff2f2f
yuki hiragino <hiraginoyuki@gmail.com>
2020-09-29 00:01:52 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: './bundle.js'
+  },
+
+  mode: 'development',
+  watch: true,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { url: false }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+    ]
+  }
+};
