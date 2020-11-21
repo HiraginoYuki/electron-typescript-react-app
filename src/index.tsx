4eeaeb80705f650fe92a0fdf7c05d2ad6f97c71b
hiragino_yuki <hiraginoyuki@gmail.com>
2020-11-21 13:34:33 +0900

    changed several things

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./app";
+import { App } from "./components/app";
+import "./global.scss";
 
-ReactDOM.render(<App />, document.querySelector("#container"));
+ReactDOM.render(<App />, document.getElementById("app"));
