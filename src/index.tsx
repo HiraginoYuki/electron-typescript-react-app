60c990c7021df7d55f43b1b5b400865fe7230cc0
hiragino_yuki <hiraginoyuki@gmail.com>
2020-11-21 13:40:12 +0900

    Revert "changed several things"
    This reverts commit 4eeaeb80705f650fe92a0fdf7c05d2ad6f97c71b.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./components/app";
-import "./global.scss";
+import { App } from "./app";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.querySelector("#container"));
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
   },
-  target: 'electron-renderer',
 
   module: {
     rules: [
